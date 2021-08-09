d8e90d310f866975c39e88a30ce4a46f1950870a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-09 17:16:36 +0700

    fix main act

--- app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/MainActivity.java
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/MainActivity.java
@@ -20,23 +20,19 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.tugas_uas_genap_2021_akb_if_9_10118377.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
