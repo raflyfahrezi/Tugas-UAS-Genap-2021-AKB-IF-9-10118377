cf97100ac1cf0ef61fed68f3accfd6aa3f896a25
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-09 14:07:47 +0700

    implement intent in splash

--- app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/SplashActivity.java
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.tugas_uas_genap_2021_akb_if_9_10118377;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
