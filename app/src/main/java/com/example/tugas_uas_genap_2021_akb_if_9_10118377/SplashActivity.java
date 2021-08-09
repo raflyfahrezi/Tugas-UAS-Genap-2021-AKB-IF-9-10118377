e93e583b586cec0ba88fbcac5ef3bd6243cf69ac
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-09 13:56:59 +0700

    create splash activity

---
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas_uas_genap_2021_akb_if_9_10118377;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
