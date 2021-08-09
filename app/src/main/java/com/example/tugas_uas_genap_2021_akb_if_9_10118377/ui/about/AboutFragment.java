fa56a561aed0d75db1df4c05ebd8dd42fb1c4ef3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-09 14:30:30 +0700

    adding about fragment_about.xml

---
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/ui/about/AboutFragment.java
@@ -0,0 +1,21 @@
+package com.example.tugas_uas_genap_2021_akb_if_9_10118377.ui.about;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tugas_uas_genap_2021_akb_if_9_10118377.R;
+
+public class AboutFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+
+        return root;
+    }
+}
