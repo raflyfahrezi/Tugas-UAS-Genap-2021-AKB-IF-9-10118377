4c91868ce53134bb13e9df18292d7ab67d57080e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-10 22:19:24 +0700

    create PlaceAdapter

---
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/ui/place/PlaceAdapter.java
@@ -0,0 +1,68 @@
+package com.example.tugas_uas_genap_2021_akb_if_9_10118377.ui.place;
+
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.tugas_uas_genap_2021_akb_if_9_10118377.R;
+import com.example.tugas_uas_genap_2021_akb_if_9_10118377.model.PlaceModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+public class PlaceAdapter extends FirebaseRecyclerAdapter<PlaceModel, PlaceAdapter.myViewHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public PlaceAdapter(@NonNull @NotNull FirebaseRecyclerOptions<PlaceModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull @NotNull myViewHolder holder, int position, @NonNull @NotNull PlaceModel model) {
+        holder.name.setText(model.getName());
+        holder.region.setText(model.getRegion());
+
+        Glide.with(holder.img.getContext())
+                .load(model.getImg())
+                .placeholder(R.drawable.common_google_signin_btn_icon_dark)
+                .error(R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_card, parent,false);
+
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView img;
+        TextView name, region;
+
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img = (ImageView) itemView.findViewById(R.id.place_image);
+            name = (TextView) itemView.findViewById(R.id.place_name);
+            region = (TextView) itemView.findViewById(R.id.place_region);
+        }
+    }
+}
