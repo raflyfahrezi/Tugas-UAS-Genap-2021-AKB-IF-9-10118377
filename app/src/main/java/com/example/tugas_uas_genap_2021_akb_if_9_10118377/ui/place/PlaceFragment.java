297ec00de3c750fbabcc162ea8d3c196ca3fcdda
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-10 22:26:56 +0700

    show data

--- app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/ui/place/PlaceFragment.java
+++ app/src/main/java/com/example/tugas_uas_genap_2021_akb_if_9_10118377/ui/place/PlaceFragment.java
@@ -14,17 +14,48 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tugas_uas_genap_2021_akb_if_9_10118377.R;
+import com.example.tugas_uas_genap_2021_akb_if_9_10118377.model.PlaceModel;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PlaceFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    PlaceAdapter placeAdapter;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        placeAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        placeAdapter.stopListening();
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_place, container, false);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.place_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        FirebaseRecyclerOptions<PlaceModel> options =
+                new FirebaseRecyclerOptions.Builder<PlaceModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("places"), PlaceModel.class)
+                        .build();
+
+        placeAdapter = new PlaceAdapter(options);
+        recyclerView.setAdapter(placeAdapter);
 
         return root;
     }
