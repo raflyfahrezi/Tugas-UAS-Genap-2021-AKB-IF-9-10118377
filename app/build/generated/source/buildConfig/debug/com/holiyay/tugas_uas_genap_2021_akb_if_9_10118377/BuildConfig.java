29c72362c97cb80a2cc2e9bce6e11a5b27b88b8e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-12 14:37:31 +0700

    update BuildConfig

--- app/build/generated/source/buildConfig/debug/com/holiyay/tugas_uas_genap_2021_akb_if_9_10118377/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/holiyay/tugas_uas_genap_2021_akb_if_9_10118377/BuildConfig.java
@@ -5,7 +5,7 @@ package com.holiyay.tugas_uas_genap_2021_akb_if_9_10118377;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.tugas_uas_genap_2021_akb_if_9_10118377";
+  public static final String APPLICATION_ID = "com.holiyay.tugas_uas_genap_2021_akb_if_9_10118377";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
