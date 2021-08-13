bb87c7fe7c7a13f7ccc1784425013e0c8ebd7c16
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-13 09:02:19 +0700

    adding new variable and its getter setter

--- app/src/main/java/com/holiyay/tugas_uas_genap_2021_akb_if_9_10118377/model/PlaceModel.java
+++ app/src/main/java/com/holiyay/tugas_uas_genap_2021_akb_if_9_10118377/model/PlaceModel.java
@@ -12,7 +12,7 @@ package com.holiyay.tugas_uas_genap_2021_akb_if_9_10118377.model;
 
 public class PlaceModel {
 
-    String img, name, region;
+    String img, name, description, region;
 
     PlaceModel() {
 
@@ -38,6 +38,14 @@ public class PlaceModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getRegion() {
